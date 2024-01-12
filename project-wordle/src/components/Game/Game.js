88ed38922609717275f851ccbc775a9a15b304cb
Santiago Báez Duic <93755514+SantiagoBaezDuic@users.noreply.github.com>
2024-01-12 01:42:52 -0300

    Fixed desync again

--- project-wordle/src/components/Game/Game.js
+++ project-wordle/src/components/Game/Game.js
@@ -3,7 +3,6 @@ import React from 'react';
 import GuessInput from '../GuessInput/GuessInput';
 import GuessTracker from '../GuessTracker/GuessTracker';
 import Banner from '../Banner/Banner';
-import Banner from '../Banner/Banner';
 import {NUM_OF_GUESSES_ALLOWED} from '../../constants';
 import {checkGuess} from "../../game-helpers";
 import { sample } from '../../utils';
@@ -11,12 +10,9 @@ import { WORDS } from '../../data';
 
 function Game() {
   //Word memory for saving past attempts.
-  //Word memory for saving past attempts.
   const [wordMemory, setWordMemory] = React.useState([{word: '', id: crypto.randomUUID(), coloringData: ['', '', '', '', '']}, {word: '', id: crypto.randomUUID(), coloringData: ['', '', '', '', '']}, {word: '', id: crypto.randomUUID(), coloringData: ['', '', '', '', '']}, {word: '', id: crypto.randomUUID(), coloringData: ['', '', '', '', '']}, {word: '', id: crypto.randomUUID(), coloringData: ['', '', '', '', '']}, {word: '', id: crypto.randomUUID(), coloringData: ['', '', '', '', '']}]);
   //State for input handling.
-  //State for input handling.
   const [currentWord, setCurrentWord] = React.useState('');
-  //State to count the amount of attempts the user made.
   //State to count the amount of attempts the user made.
   const [currentTrys, setCurrentTrys] = React.useState(0);
   //Boolean to define if the input is disabled or not.
@@ -41,27 +37,19 @@ function Game() {
   function tryCurrentWord() {
     //Prevents the game from testing the word if the game is over.
     if(isGameOver) {
-    //Prevents the game from testing the word if the game is over.
-    if(isGameOver) {
       return;
     }
 
     //Tests the word.
-    //Tests the word.
     const results = checkGuess(currentWord, answer).map((el) => el.status);
 
-    //Saves the word and the coincidences in memory.
     //Saves the word and the coincidences in memory.
     let nextWordMemory = [...wordMemory];
     nextWordMemory[currentTrys] = {...nextWordMemory[currentTrys], word: currentWord, coloringData: results};
     setWordMemory(nextWordMemory);
 
     //Adds a new try.
-
-    //Adds a new try.
     setCurrentTrys(currentTrys + 1);
-
-    //Cleans the input.
 
     //Cleans the input.
     setCurrentWord('');
